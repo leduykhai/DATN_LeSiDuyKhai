91952fa3fb76b82c6d40d0d5d210d6b33524b15d
LESIDUYKHAI <leduykhai00@gmail.com>
2022-04-20 21:36:36 +0700

    up

--- server/api/Routes/chitietluutrus.routes.js
+++ server/api/Routes/chitietluutrus.routes.js
@@ -2,13 +2,13 @@ module.exports = function (app) {
     let chitietluutrusCtrl = require('../Controllers/chitietluutrusControllers')
 
     //ChiTietLuuTru
-    app.route('/luutrus')
+    app.route('/chitietluutrus')
         .get(chitietluutrusCtrl.get)
         .post(chitietluutrusCtrl.addNewChiTietLuuTru)
         .put(chitietluutrusCtrl.updateChiTietLuuTru)
         .delete(chitietluutrusCtrl.deleteChiTietLuuTruByAll);
 
-    app.route('/luutrus/:ma_chi_tiet_lt')
+    app.route('/chitietluutrus/:ma_chi_tiet_lt')
         .get(chitietluutrusCtrl.getChiTietLuuTruById)
         .delete(chitietluutrusCtrl.deleteChiTietLuuTruById);
 
